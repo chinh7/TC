3ab49645d229e6c640762fd4b2f40a4335ce43ad
Chinh Nguyen <chinh7@me.com>
2013-11-18 17:25:21 +0900

    usaco

---
+++ src/usaco/Msched.java
@@ -0,0 +1,11 @@
+package usaco;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chinh
+ * Date: 11/18/13
+ * Time: 4:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Msched {
+}
